b609eb6754e5cbb71bd75535fa5bb6640f60fef6
AniX2212 <shchur_ak18@nuwm.edu.ua>
2024-01-29 13:23:15 +0200

    host fix

--- Storage.WebApi/obj/Debug/net7.0/Storage.WebApi.AssemblyInfo.cs
+++ Storage.WebApi/obj/Debug/net7.0/Storage.WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Storage.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db7a0e213b5ba9a266fbf66b4d8b081f1d5df556")]
 [assembly: System.Reflection.AssemblyProductAttribute("Storage.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Storage.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
